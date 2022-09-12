7827a2443ee7b461e80ceacb012d77012a651b97
miraz <hasibul.miraz@northsouth.edu>
2022-09-12 13:13:22 +0600

    initial commit, working on file system

---
+++ source/main.py
@@ -0,0 +1,4 @@
+from mytimer import MyTimer
+
+x = MyTimer("hello")
+MyTimer.check_file()
---
+++ source/mytimer.py
@@ -0,0 +1,18 @@
+import os
+class MyTimer:
+    def __init__(self, start_time):
+        self.start_time = start_time
+    
+    def start_write():
+        pass
+
+    def save_write():
+        pass
+
+    def stop_write():
+        pass
+
+    def check_file():
+        path = "data.txt"
+        if not os.path.exists("data.txt"):
+            open("data.txt","x")
